0091ce38b7df78bf96be1a87084c43929cae2704
Marvin Hagemeister <marvin@marvinhagemeister.de>
2017-06-08 23:54:20 +0200

    Proof of concept

--- src/__tests__/index.spec.ts
+++ src/__tests__/index.spec.ts
@@ -1,8 +1,79 @@
-import { assert as t } from "chai";
+import { assert as t } from "chai";
+import Benchmark, { Result } from "../index";
 
-describe("foo", () => {
-  it("should return foo", () => {
-    const foo = () => "foo";
-    t.equal(foo(), "foo");
+function normalize(res: Result) {
+  if (res.displayTime.length > 3) {
+    res.displayTime = "100 ms";
+  }
+
+  if (res.time > 0) {
+    res.time = 1;
+  }
+  return res;
+}
+
+function delay(ms: number) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+describe("Benchmark", () => {
+  it("should run test cases", async () => {
+    const bench = await new Benchmark(2)
+      .add("test1", () => 1)
+      .add("test2", () => 2)
+      .run();
+
+    const res = bench.results.map(normalize);
+
+    t.deepEqual(res, [
+      {
+        displayTime: "100 ms",
+        name: "test1",
+        time: 1,
+      },
+      {
+        displayTime: "100 ms",
+        name: "test2",
+        time: 1,
+      },
+    ]);
+  });
+
+  it("should run tests asynchronously", async () => {
+    const bench = await new Benchmark(2)
+      .add("test1", () => delay(1))
+      .add("test2", () => delay(2))
+      .run();
+
+    const res = bench.results.map(normalize);
+
+    t.deepEqual(res, [
+      {
+        displayTime: "100 ms",
+        name: "test1",
+        time: 1,
+      },
+      {
+        displayTime: "100 ms",
+        name: "test2",
+        time: 1,
+      },
+    ]);
+  });
+
+  it("should print result", async () => {
+    const bench = await new Benchmark(2)
+      .add("test2", () => 2)
+      .add("test1", () => 1)
+      .run();
+
+    let result = "";
+    const logger = (msg?: string) => {
+      result += msg + "\n";
+    };
+
+    bench.print(logger);
+
+    t.equal(/Fastest/g.test(result), true);
   });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,79 @@
-// tslint:disable-next-line
-console.log("hello world");
+/* tslint:disable:ban-types curly no-var-requires */
+import * as chalk from "chalk";
+const now = require("performance-now");
+
+export interface Case {
+  f: () => any;
+  name: string;
+}
+
+export interface Result {
+  name: string;
+  time: number;
+  displayTime: string;
+}
+
+export default class Benchmark {
+  cases: Case[] = [];
+  results: Result[] = [];
+  iter: number;
+
+  constructor(iter: number = 1000) {
+    this.iter = iter;
+  }
+
+  add(name: string, f: () => any) {
+    this.cases.push({ name, f });
+    return this;
+  }
+
+  async run(iterations: number = 1000): Promise<Benchmark> {
+    // Warmup
+    for (let i = 0; i < 10; i++) {
+      for (const item of this.cases) {
+        item.f();
+      }
+    }
+
+    const p = Promise.resolve();
+
+    for (const test of this.cases) {
+      const runs = [];
+      for (let i = 0; i < this.iter; i++) {
+        runs.push(true);
+      }
+
+      let start = 0;
+      p.then(() => (start = now()));
+      runs.map(run => p.then(() => test.f.call(null)));
+      p.then(() => {
+        const time = now() - start;
+
+        this.results.push({
+          name: test.name,
+          time,
+          displayTime: time.toFixed(5) + " ms",
+        });
+      });
+    }
+
+    await p;
+    return this;
+  }
+
+  print(logger: (msg?: string) => any) {
+    logger();
+
+    this.results.map(res =>
+      logger(res.name + ": " + chalk.yellow(res.displayTime)),
+    );
+
+    this.results = this.results.sort((a, b) => {
+      if (a.time < b.time) return -1;
+      if (a.time > b.time) return 1;
+      return 0;
+    });
+
+    logger("\nFastest: " + chalk.green(this.results[0].name));
+  }
+}
