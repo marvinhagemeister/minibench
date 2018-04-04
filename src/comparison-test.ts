e553ace0e217922ef302a8da731f677590d0da9d
Marvin Hagemeister <marvin@marvinhagemeister.de>
2018-04-05 01:10:53 +0200

    Add recursive fibonacci to comparisons

--- src/comparison-test.ts
+++ src/comparison-test.ts
@@ -7,12 +7,14 @@ export const delay = (ms: number) =>
 export async function perf() {
   await bench("foo", () => 1 + 1);
   await bench("foo2", () => 1 ** 1);
-  await bench("fibonacci", () => fibonacci(3));
+  await bench("fibonacci", () => fibonacci(10));
+  await bench("recfibonacci", () => recursiveFibonacci(10));
   await bench("foo3", () => delay(2));
   await new Benchmark("pretty")
     .add("foo", () => 1 + 1)
     .add("foo2", () => 1 ** 1)
-    .add("fibonacci", () => fibonacci(3))
+    .add("fibonacci", () => fibonacci(10))
+    .add("rec fibonacci", () => recursiveFibonacci(10))
     .add("foo3", () => delay(2))
     .run();
 }
@@ -32,4 +34,9 @@ function fibonacci(num: number) {
   }
 
   return b;
+}
+
+function recursiveFibonacci(n: number): number {
+  if (n === 1 || n === 2) return 1;
+  return recursiveFibonacci(n - 1) + recursiveFibonacci(n - 2);
 }
