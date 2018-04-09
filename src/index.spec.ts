34420ef0903f4426ca3d504289f43c5cece74334
Marvin Hagemeister <marvin@marvinhagemeister.de>
2018-04-09 21:55:05 +0200

    Fix tests

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -5,11 +5,19 @@ function delay(ms: number) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
+function work(n: number) {
+  let result = 0;
+  for (let i = 0; i < n; i++) {
+    result = i + 1;
+  }
+  return result;
+}
+
 describe("Benchmark", () => {
   it("should run test cases", async () => {
     const res = [];
-    res[0] = await benchmark("foo", () => 1);
-    res[1] = await benchmark("foo2", () => 2);
+    res[0] = await benchmark("foo", () => work(1));
+    res[1] = await benchmark("foo2", () => work(10));
     t.equal(res[0].hz > res[1].hz, true);
   });
 
